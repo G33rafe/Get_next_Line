ef76af0f5e1baebfe13149b505f8f966385044b6
GEErafe <raphaeljacques5@gmail.com>
2022-08-30 22:02:11 -0400

    bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rajacque <rajacque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 14:27:19 by rajacque          #+#    #+#             */
+/*   Updated: 2022/08/30 14:27:19 by rajacque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_new_line(char *buffer)
+{
+	size_t	i;
+	char	*new_line;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	new_line = ft_calloc(sizeof(char), (ft_strlen(buffer) + 1));
+	if (!new_line)
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		new_line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+		new_line[i] = buffer[i];
+	return (new_line);
+}
+
+char	*ft_new_buffer(char *buffer)
+{
+	size_t	i;
+	size_t	j;
+	char	*new_buffer;
+
+	i = 0;
+	j = 0;
+	while (buffer[i] != '\n' && buffer[i])
+		i++;
+	if (!buffer[i])
+	{
+		free (buffer);
+		return (NULL);
+	}
+	new_buffer = ft_calloc(sizeof(char), (ft_strlen(buffer) - i + 1));
+	while (buffer[i++])
+		new_buffer[j++] = buffer[i];
+	free (buffer);
+	return (new_buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[FOPEN_MAX];
+	char		*line;
+	int			read_value;
+
+	read_value = 1;
+	if (fd < 0 || BUFFER_SIZE < 1 || fd > FOPEN_MAX)
+		return (NULL);
+	line = ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+	if (!line)
+		return (NULL);
+	while (read_value != 0 && !ft_strchr(buffer[fd], '\n'))
+	{
+		read_value = read(fd, line, BUFFER_SIZE);
+		if (read_value == -1)
+		{
+			free (line);
+			return (NULL);
+		}
+		line[read_value] = '\0';
+		buffer[fd] = ft_strjoin(buffer[fd], line);
+	}
+	free (line);
+	line = ft_new_line(buffer[fd]);
+	buffer[fd] = ft_new_buffer(buffer[fd]);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rajacque <rajacque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 14:18:09 by rajacque          #+#    #+#             */
+/*   Updated: 2022/08/30 14:18:09 by rajacque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdint.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s1);
+void	*ft_calloc(size_t nbr, size_t size);
+
+#endif
